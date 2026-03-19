053991ced98eafc4c82ad5da47d60656e805aa27
ritwikrazneesh <63254416+ritwikrazneesh@users.noreply.github.com>
2026-03-20 03:47:58 +0530

    Add TransformDataset to __all__ exports

--- src/rcdp/data/__init__.py
+++ src/rcdp/data/__init__.py
@@ -1,9 +1,11 @@
 from .datasets import DatasetSpec, build_dataset
 from .fewshot import FewShotSplit, make_fewshot_split
+from .apply_transform import TransformDataset
 
 __all__ = [
     "DatasetSpec",
     "build_dataset",
     "FewShotSplit",
     "make_fewshot_split",
+    "TransformDataset",
 ]
